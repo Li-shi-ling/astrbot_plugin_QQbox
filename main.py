713ae289bf77e60f1fcc1a8d6e900b15efe554f4
Lishiling <m18384519631@163.com>
2026-02-24 12:14:09 +0800

    Revert "fix:修改qqbox在设置"
    This reverts commit 3927dd4347571240ab5e587f4db198c3140bf48d.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from astrbot.api import AstrBotConfig, logger
 from PIL import Image, ImageDraw, ImageFont, ImageSequence
 from astrbot.api.star import StarTools
 from io import BytesIO
+import traceback
 import aiofiles
 import aiohttp
 import asyncio
@@ -1078,7 +1079,7 @@ class ChatBubbleGenerator:
         ]
 
         # 如果有头衔，调整宽度
-        if title_info and (not title_info.get("content", None) is None):
+        if title_info and title_info.get("content", None):
             title_width = draw.textlength(
                 title_info.get("content", ""),
                 font=self.title_font
@@ -1123,9 +1124,8 @@ class ChatBubbleGenerator:
 
         if title_info and title_info.get("content", None):
             # 处理头衔
-            t_c = title_info.get("color", None)
             title_color = self.color_map.get(
-                int(1 if t_c is None else t_c),
+                int(title_info.get("color", 1)),
                 self.color_map[1]
             )
             title_content = title_info.get("content", "")
